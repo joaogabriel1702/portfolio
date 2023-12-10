517432e2fb2e9dc3c54aa27ba7ea7c4e597a2a3e
joaogabriel1702 <joaogabrielcsoares@hotmail.com>
2023-12-09 23:07:33 -0300

    ajuste na tela de experiencia

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -8,7 +8,7 @@ function displayData(selectedValue) {
     } else if (selectedValue === "3") {
         contentArea.innerHTML = "<h3>New School - Educação</h3><p>Desenvolvedor FullStack Voluntário</p><ul><li>Desenvolvimento de telas e funcionalidades no front-end</li><li>Resolução de bugs no Back-End</li></ul><p>Ferramentas: Git, JavaScript, Vue.js, Typescript, Kanba, Scrum.</p>";
     } else if (selectedValue === "4") {
-        contentArea.innerHTML = "<h3>Centro Hospitalar Albert Sabin - Hospitalar</h3><p>Jovem Aprendiz - Auxiliar de TI</p><ul><li>Criação de uma intranet com PHP, Drupal e banco de dados da Oracle</li><li>Manutenção de Máquinas</li><li>Auxílio para os funcionários que utilizavam o sistema DGS</li><li>Auxílio remoto via AnyDesk</li></ul><p>Ferramentas: Git, Trello, PHP, Oracle SQL, Drupal.</p>";
+        contentArea.innerHTML = "<h3>Centro Hospitalar Albert Sabin</h3><p>Jovem Aprendiz - Auxiliar de TI</p><ul><li>Criação de uma intranet com PHP, Drupal e banco de dados da Oracle</li><li>Manutenção de Máquinas</li><li>Auxílio para os funcionários que utilizavam o sistema DGS</li><li>Auxílio remoto via AnyDesk</li></ul><p>Ferramentas: Git, Trello, PHP, Oracle SQL, Drupal.</p>";
     } else {
         contentArea.innerHTML = "";
     }
